ab9a1884dfa60ea126573c23b3441a66d1c00fd6
DesSolo <dessolo@mail.ru>
2020-09-07 09:18:38 +0400

    update cors

--- capsula/middlewares.go
+++ capsula/middlewares.go
@@ -6,8 +6,8 @@ import "net/http"
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if r.Method == http.MethodOptions {
 			return
 		}
